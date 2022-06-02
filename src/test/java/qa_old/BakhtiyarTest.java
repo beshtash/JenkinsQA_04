f87280b2e36f4827d4774d61438d4fc0e1b01402
Dastan Torokul <talantd34@gmail.com>
2022-06-02 16:51:51 -0500

    Add tests

---
+++ src/test/java/qa_old/BakhtiyarTest.java
@@ -0,0 +1,26 @@
+package qa_old;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class BakhtiyarTest extends BaseTest {
+
+    @Test
+    public void bakhtiyarMametyarov() {
+        getDriver().get("https://habr.com/ru/all/");
+        WebElement searchButton = getDriver().findElement(By.xpath("//a[@href=\"/ru/search/\"]"));
+        searchButton.click();
+
+        WebElement searchBox = getDriver().findElement(By.className("tm-input-text-decorated__input"));
+        searchBox.sendKeys("Основы Postman для самых маленьких"+ "\n");
+        searchButton = getDriver().findElement(By.xpath("(//a[@class='tm-article-snippet__title-link'])[1]"));
+        searchButton.click();
+
+        String text = getDriver().findElement(By.xpath("//article//h1/span")).getText();
+        Assert.assertEquals(text, "Основы Postman для самых маленьких");
+    }
+
+}
---
+++ src/test/java/qa_old/JuliaSabDropDownListTest.java
@@ -0,0 +1,26 @@
+package qa_old;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class JuliaSabDropDownListTest extends BaseTest {
+
+    public void setBirthDay(String day, String month, String year) {
+        getDriver().get(JuliaSabConstantsUtils.MAIL_REGISTRATION);
+
+        getDriver().findElement(By.xpath(JuliaSabConstantsUtils.BIRTH_DAY_XPATH)).click();
+        getDriver().findElement(By.xpath(String.format(JuliaSabConstantsUtils.XPATH_FOR_INPUT_TEXT, day))).click();
+
+        getDriver().findElement(By.xpath(JuliaSabConstantsUtils.MONTH_XPATH)).click();
+        getDriver().findElement(By.xpath(String.format(JuliaSabConstantsUtils.XPATH_FOR_INPUT_TEXT, month))).click();
+
+        getDriver().findElement(By.xpath(JuliaSabConstantsUtils.YEAR_XPATH)).click();
+        getDriver().findElement(By.xpath(String.format(JuliaSabConstantsUtils.XPATH_FOR_INPUT_TEXT, year))).click();
+    }
+
+    @Test
+    public void inputBirthday() {
+        setBirthDay("27", "Январь", "1997");
+    }
+}
